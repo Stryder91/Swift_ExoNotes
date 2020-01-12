2af496d679641502353a9c935cb3a07895110afc
lil <lionel.tran@eemi.com>
2020-01-12 22:31:42 +0100

    Init

---
+++ Sources/KituraTest/main.swift
@@ -0,0 +1,85 @@
+import Kitura
+import HeliumLogger
+import KituraStencil
+
+import Foundation
+
+HeliumLogger.use()
+
+let router = Router()
+
+router.all(middleware: BodyParser())
+router.all(middleware: [BodyParser(), StaticFileServer(path: "./Public")])
+router.add(templateEngine: StencilTemplateEngine())
+
+let classe : [String : [Int] ] = [
+    "Matheus" : [16,14,20,14,12],
+    "Lionel" : [16,14,20,14,12],
+    "Matt" : [16,14,20,14,12],
+    "Lili": [16,14,20,14,12],
+    "Bob" : [16,14,20,14,12],
+    "Xavier" : [16,14,20,14,12],
+    "Bip" : [16,14,20,14,12],
+    "Bobi" : [16,14,20,14,12],
+    "Bernard" : [16,14,20,14,12],
+    "Matthieu" : [1,18,12,17,5,19],
+]
+
+router.get("/") { request, response, next in
+    try response.render("Home.stencil", context: ["greeting" : "hello"])
+    next()
+}
+
+router.post("/eleves") { request, response, next in
+    if let body = request.body?.asURLEncoded { 
+        if let name = body["name"]{
+            print(name)
+            var eleves : [String] = []
+            
+            for (eleve, note) in classe {
+                if eleve.contains(name) {
+                    print("exists", eleve)
+                    eleves.append(eleve)
+                }
+            } 
+            for eleve in eleves {
+                print(eleve)
+            }
+        try response.render("Class.stencil", context: ["class" : eleves])
+        }
+        //response.send("<li><form action='/eleve' method='get'><input type='hidden' value'\(eleve)' placeholder='\(eleve)' name='name'/> <button type='submit'>\(eleve)</button></form></li>")
+    next()
+    }
+    else {
+       response.status(.notFound)
+    }
+    next()
+}
+router.get("/eleves/:name") { request, response, next in
+    var mediane : Int
+    var ecart_type : Double = 0
+    var variance : Int = 0
+    var sous_variance : Int 
+    var moyenne : Int = 0
+    var notes : [Int] = []
+    if let name = request.parameters["name"]{
+        notes = classe[name] ?? [10,10]
+        for note in notes {
+            moyenne += note
+        }
+        var tableauTrie : [Int] = notes.sorted()
+        moyenne = moyenne / notes.count
+        for element in tableauTrie {
+            sous_variance = element - moyenne
+            print(variance)
+            variance += (sous_variance * sous_variance)
+        }
+        ecart_type = Double(variance).squareRoot()
+        mediane = tableauTrie[tableauTrie.count / 2]
+        try response.render("Bulletin.stencil", context: ["bulletin" : notes, "eleve": name, "moyenne":moyenne, "mediane":mediane, "ecart_type":ecart_type])
+    }
+}
+
+
+Kitura.addHTTPServer(onPort: 8080, with: router)
+Kitura.run()
---
+++ Tests/LinuxMain.swift
@@ -0,0 +1,7 @@
+import XCTest
+
+import KituraTestTests
+
+var tests = [XCTestCaseEntry]()
+tests += KituraTestTests.allTests()
+XCTMain(tests)
