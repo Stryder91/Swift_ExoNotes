9fee87419246cd3b644f703c4918271debf7ae0a
lil <lionel.tran@eemi.com>
2020-01-13 11:16:14 +0100

    Refactor et créations de fonctions pour contenir moyenne, médiane et ecart-type

--- Sources/KituraTest/main.swift
+++ Sources/KituraTest/main.swift
@@ -17,7 +17,7 @@ let classe : [String : [Int] ] = [
     "Lionel" : [16,14,10,11,12],
     "Matt" : [8,14,19,14,10],
     "Lili": [6,10,17,12,12],
-    "Bob" : [11,14,10,13,8],
+    "Bob" : [11,10,11,11,14],
     "Xavier" : [4,10,11,14,6],
     "Bip" : [5,14,14,6,18],
     "Bobi" : [6,12,9,11,13],
@@ -26,28 +26,22 @@ let classe : [String : [Int] ] = [
 ]
 
 router.get("/") { request, response, next in
-    try response.render("Home.stencil", context: ["greeting" : "hello"])
+    try response.render("Home.stencil", context: ["greeting" : "Bienvenue dans la fameuse classe"])
     next()
 }
 
 router.post("/eleves") { request, response, next in
     if let body = request.body?.asURLEncoded { 
         if let name = body["name"]{
-            print(name)
             var eleves : [String] = []
             
             for (eleve, note) in classe {
                 if eleve.contains(name) {
-                    print("exists", eleve)
                     eleves.append(eleve)
                 }
             } 
-            for eleve in eleves {
-                print(eleve)
-            }
         try response.render("Class.stencil", context: ["class" : eleves])
         }
-        //response.send("<li><form action='/eleve' method='get'><input type='hidden' value'\(eleve)' placeholder='\(eleve)' name='name'/> <button type='submit'>\(eleve)</button></form></li>")
     next()
     }
     else {
@@ -55,28 +49,45 @@ router.post("/eleves") { request, response, next in
     }
     next()
 }
-router.get("/eleves/:name") { request, response, next in
-    var mediane : Int
-    var ecart_type : Double = 0
-    var variance : Int = 0
-    var sous_variance : Int 
+func get_moyenne(notes: [Int]) -> Int{
     var moyenne : Int = 0
-    var notes : [Int] = []
-    if let name = request.parameters["name"]{
-        notes = classe[name] ?? [10,10]
-        for note in notes {
-            moyenne += note
-        }
-        var tableauTrie : [Int] = notes.sorted()
-        moyenne = moyenne / notes.count
-        for element in tableauTrie {
-            sous_variance = element - moyenne
-            print(variance)
+    for note in notes {
+        moyenne += note
+    }
+    moyenne = moyenne / notes.count
+    return moyenne
+}
+func get_mediane(notes: [Int]) -> Int {
+    var mediane : Int
+    //Tri du tableau afin de trouver la médiane
+    var tableauTrie : [Int] = notes.sorted()
+    mediane = tableauTrie[tableauTrie.count / 2]
+    return mediane
+}
+func get_ecartType(notes: [Int], moyenne: Int) -> Double {
+    var sous_variance : Int
+    var variance : Int = 0
+    var ecart_type : Double = 0
+    for note in notes {
+            sous_variance = note - moyenne
+            print(sous_variance)
             variance += (sous_variance * sous_variance)
         }
-        ecart_type = Double(variance).squareRoot()
-        mediane = tableauTrie[tableauTrie.count / 2]
-        try response.render("Bulletin.stencil", context: ["bulletin" : notes, "eleve": name, "moyenne":moyenne, "mediane":mediane, "ecart_type":ecart_type])
+    ecart_type = Double(variance).squareRoot()
+    return ecart_type
+}
+router.get("/eleves/:name") { request, response, next in
+    //Déclarations
+    var notes : [Int] = []
+    var moyenne : Int = 0
+
+    if let name = request.parameters["name"]{
+        notes = classe[name]! 
+        //On passe les appels aux fonctions directement dans le context
+        try response.render("Bulletin.stencil", context: ["bulletin" : notes, "eleve": name, 
+        "moyenne":get_moyenne(notes:notes), 
+        "mediane":get_mediane(notes:notes), 
+        "ecart_type":get_ecartType(notes:notes, moyenne:get_moyenne(notes:notes))])
     }
 }
 
